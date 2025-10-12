778df1f89ec20c24e622e95aee78bf1f12af995d
vanikkars <vanik888msk@gmail.com>
2025-10-12 14:53:46 +0200

    created an ingestion pipeline

--- airflow/dags/extract_audit_logs_with_time_interval.py
+++ airflow/dags/extract_audit_logs_with_time_interval.py
@@ -1,27 +1,42 @@
 # dags/audit_log_extract_dag.py
-from __future__ import annotations
-import os
+
 import csv
 import pendulum
 import logging
-from airflow.sdk import dag, task, get_current_context
+from airflow.sdk import dag, task
 from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 import pendulum as pnd
 from airflow.timetables.interval import DeltaDataIntervalTimetable
+from airflow.providers.trino.hooks.trino import TrinoHook
+from airflow import settings
 
 S3_CONN_ID = "s3_conn"
-PG_CONN_ID = "ecom_audit_logs"          # Define this connection in Airflow UI
+PG_CONN_ID = "ecom_audit_logs"
+TRINO_CONN_ID = "trino_conn"
 
 DAG_ID = "audit_log_extract"
 LAST_TS_VAR = f"{DAG_ID}_last_audit_timestamp"
 OUTPUT_DIR = "/opt/airflow/data/extracts"
-BATCH_SIZE = 3
+EXTRACT_BATCH_SIZE = 3
+INGEST_BATCH_SIZE = 3
+
+ICEBERG_DB = "landing"
+ICEBERG_RAW_JSON_TABLE = "audit_log_dml"          # 3-column raw capture
+DBT_PROJECT_PATH = f"{settings.DAGS_FOLDER}/dbt_dwh"
+
+
 
 S3_BUCKET_NAME = "lake"
 S3_PREFIX = "raw/ecommerce/orders"
+
+
+def to_timestamptz_literal(ts: str) -> str:
+    # Parse, normalize to UTC, format microseconds
+    dt = pendulum.parse(ts).in_timezone("UTC")
+    return f"TIMESTAMP '{dt.format('YYYY-MM-DD HH:mm:ss.SSSSSS')} UTC'"
 
 
 @dag(
@@ -42,7 +57,7 @@ def audit_log_extract_with_data_intervals_dag():
         logger = logging.getLogger("airflow.task")
         hook = PostgresHook(postgres_conn_id=PG_CONN_ID)
         sql = """
-            SELECT event_id,
+            SELECT audit_event_id,
                    audit_operation,
                    audit_timestamp,
                    tbl_schema,
@@ -64,7 +79,7 @@ def audit_log_extract_with_data_intervals_dag():
         extracted_files = []
         offset = 0
         iteration = 0
-        batch_size = BATCH_SIZE
+        batch_size = EXTRACT_BATCH_SIZE
         while True:
             iteration += 1
             formatted_sql = sql.format(start_date, end_date, batch_size, offset)
@@ -79,7 +94,7 @@ def audit_log_extract_with_data_intervals_dag():
             if rows:
                 s3_hook = S3Hook(aws_conn_id=S3_CONN_ID)
 
-                header = ["event_id", "audit_operation", "audit_timestamp", "tbl_schema", "tbl_name", "raw_data"]
+                header = ["audit_event_id", "audit_operation", "audit_timestamp", "tbl_schema", "tbl_name", "raw_data"]
                 csv_buffer = StringIO()
                 csv_writer = csv.writer(csv_buffer)
                 csv_writer.writerow(header)
@@ -110,24 +125,97 @@ def audit_log_extract_with_data_intervals_dag():
             #     # No data; advance marker to end_dt to avoid re-scanning
             #     Variable.set(LAST_TS_VAR, end_date.isoformat())
 
+    # drop_table = SQLExecuteQueryOperator(
+    #     task_id="drop_table",
+    #     conn_id=TRINO_CONN_ID,
+    #     sql=f"""
+    #     DROP TABLE IF EXISTS {ICEBERG_DB}.{ICEBERG_RAW_JSON_TABLE}
+    #     """
+    # )
 
-        create_iceberg_raw_json = SQLExecuteQueryOperator(
-            task_id="create_iceberg_rides_raw_json",
-            conn_id=TRINO_CONN_ID,
-            sql=f"""
-            CREATE TABLE IF NOT EXISTS {ICEBERG_DB}.{ICEBERG_RAW_JSON_TABLE} (
-                ingested_at   timestamp,
-                source_file  varchar,
-                raw_data     varchar
-            )
-            WITH (
-                format='PARQUET',
-                partitioning=ARRAY['day(ingested_at)']
-            )
-            """
+    create_iceberg_raw_json_tbl = SQLExecuteQueryOperator(
+        task_id="create_iceberg_raw_json",
+        conn_id=TRINO_CONN_ID,
+        sql=f"""
+        CREATE TABLE IF NOT EXISTS {ICEBERG_DB}.{ICEBERG_RAW_JSON_TABLE} (
+            ingested_at      timestamp(6) with time zone,
+            source_file      varchar,
+            audit_event_id   bigint,
+            audit_operation  varchar,
+            audit_timestamp  timestamp(6) with time zone,
+            tbl_schema       varchar,
+            tbl_name         varchar,
+            raw_data         varchar
         )
+        WITH (
+            format='PARQUET',
+            partitioning=ARRAY['day(ingested_at)']
+        )
+        """
+    )
 
-    extract()
+
+    @task
+    def load_raw_jsons_to_iceberg(s3_key: str):
+        from io import StringIO
+
+        logger = logging.getLogger("airflow.task")
+        s3 = S3Hook(aws_conn_id=S3_CONN_ID)
+        content = s3.read_key(key=s3_key, bucket_name=S3_BUCKET_NAME)
+        csv_buffer = StringIO(content)
+        reader = csv.DictReader(csv_buffer)
+        records = list(reader)
+        if not records:
+            logger.info("No records to load.")
+
+        trino = TrinoHook(trino_conn_id=TRINO_CONN_ID)
+        batch_size = INGEST_BATCH_SIZE
+        total = 0
+
+        for i in range(0, len(records), batch_size):
+            batch = records[i:i + batch_size]
+            values = []
+            for r in batch:
+                audit_event_id = r.get('audit_event_id')
+                audit_operation = r.get('audit_operation')
+                audit_timestamp = r.get('audit_timestamp')
+                tbl_schema = r.get('tbl_schema')
+                tbl_name = r.get('tbl_name')
+                raw_data = r.get('raw_data')
+
+                audit_ts_literal = to_timestamptz_literal(audit_timestamp)
+
+                logger.info(
+                    f"trying to insert the row: "
+                    f"{audit_event_id}, {audit_operation}, {audit_timestamp}, {tbl_schema}, {tbl_name}, {raw_data}"
+                )
+                values.append(
+                    "("
+                    "CAST(current_timestamp AS timestamp(6) with time zone),"  # ingested_at
+                    f"'{s3_key}',"  # source_file
+                    f"{audit_event_id},"  # audit_event_id
+                    f"'{audit_operation}',"  # audit_operation
+                    f"{audit_ts_literal},"  # audit_timestamp (NOT quoted)
+                    f"'{tbl_schema}',"
+                    f"'{tbl_name}',"
+                    f"'{raw_data}'"
+                    ")"
+                )
+            sql = (
+                f"INSERT INTO {ICEBERG_DB}.{ICEBERG_RAW_JSON_TABLE} "
+                f"(ingested_at, source_file, audit_event_id, audit_operation, audit_timestamp, tbl_schema, tbl_name, raw_data) "
+                f"VALUES "
+            ) + ",".join(values)
+            logger.info(f'Running the insert SQL: {sql}')
+            trino.run(sql=sql)
+            total += len(batch)
+            logger.info(f"Inserted batch={len(batch)} total={total}")
+        logger.info(f"Finished loading {total} rows into {ICEBERG_DB}.{ICEBERG_RAW_JSON_TABLE}")
+        return total
+
+    raw_s3_keys = extract()
+
+    create_iceberg_raw_json_tbl >> load_raw_jsons_to_iceberg.expand(s3_key=raw_s3_keys)
 
 
 dag_instance = audit_log_extract_with_data_intervals_dag()
--- ecommerce-db/db/init/create-tables.sql
+++ ecommerce-db/db/init/create-tables.sql
@@ -1,8 +1,13 @@
+
 CREATE TABLE IF NOT EXISTS audit_log_dml (
-    event_id        SERIAL PRIMARY KEY,
+    audit_event_id  SERIAL PRIMARY KEY,
     audit_operation TEXT,
-    audit_timestamp TIMESTAMPTZ,
+    audit_timestamp TIMESTAMP WITH TIME ZONE,
     tbl_schema      TEXT,
     tbl_name        TEXT,
     raw_data        TEXT
 );
+
+CREATE INDEX IF NOT EXISTS idx_audit_log_dml_event_id ON audit_log_dml(audit_event_id);
+CREATE INDEX IF NOT EXISTS idx_audit_log_dml_audit_timestamp ON audit_log_dml(audit_timestamp);
+
